1f4e5468be17f43eb24a2b5d0dd289938d9f421f
KaveenGandhi <60707182+KaveenGandhi@users.noreply.github.com>
2021-09-08 09:46:31 +0530

    Add files via upload

---
+++ Model/heart_disease_app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pickle 
+
+app = Flask(__name__)
+
+filename = 'model.pkl'
+
+# load model first 
+loaded_model = pickle.load(open(filename, 'rb'))
+
+
+@app.route('/')
+def home():
+	return render_template('heart_predict.html')
+
+# Bind predict function to URL
+@app.route(‘/predict’, methods =[‘POST’])
+def predict():
+ 
+    # Put all form entries values in a list 
+    features = [float(i) for i in request.form.values()]
+    # Convert features to array
+    array_features = [np.array(features)]
+    # Predict features
+    prediction = model.predict(array_features)
+ 
+    output = prediction
+ 
+    # Check the output values and retrieve the result with html tag based on the value
+ if output == 1:
+    return render_template(‘Heart Disease Classifier.html’,result = ‘The patient is not likely to have heart disease!’)
+ else:
+    return render_template(‘Heart Disease Classifier.html’,result = ‘The patient is likely to have heart disease!’)
+
+if __name__ == ‘__main__’:
+#Run the application
+app.run()
